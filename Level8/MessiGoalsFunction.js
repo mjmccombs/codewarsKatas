b4a063018596a151605310b56e3d6190087ed5f0
Matt McCombs <mattmccombs62097@gmail.com>
2019-10-02 12:21:07 +0200

    more level 8 katas

---
+++ Level8/MessiGoalsFunction.js
@@ -0,0 +1,15 @@
+/*
+Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions.
+Complete the goals function to return his total goals (the sum) for all three leagues.
+
+Note: the parameter for this function will always be a valid number.
+
+*/
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+	return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+}
